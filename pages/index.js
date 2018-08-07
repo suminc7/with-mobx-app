9981a5fa148a7d29d1129cfb65ce49c8955b6872
bran <suminc7@naver.com>
2018-08-07 17:42:31 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import React from 'react'
 import Page from '../components/Page'
+import { observable, action } from 'mobx'
+import { observer, inject, Provider } from 'mobx-react'
 
-export default class Counter extends React.Component {
-  render () {
-    return (
-      <Page title='Index Page' linkTo='/other' />
-    )
-  }
+
+
+class CountStore {
+    @observable count = 0;
+
+    @action
+    increase = () => {
+        this.count++;
+    }
+
+    @action
+    decrease = () => {
+        this.count--;
+    }
+}
+
+const store = new CountStore();
+
+@inject('store')
+@observer
+class Counter extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        const { store } = this.props;
+        return (
+            <div>
+                <button onClick={() => store.decrease()}>-</button>
+                { store.count }
+                <button onClick={() => store.increase()}>+</button>
+            </div>
+        )
+    }
+}
+
+
+export default class Index extends React.Component {
+    render () {
+        return (
+            <div>
+                <Page title='Index Page' linkTo='/other' />
+                <Provider store={store}>
+                    <Counter/>
+                </Provider>
+            </div>
+        )
+    }
 }
